766c32c27ec857d52c68f4892274279a34cb2ab4
ValarMorghulis <alexandrtcacenco324@gmail.com>
2024-02-05 04:11:24 +0200

    The Exam Special

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,135 @@ export default function Home():React.ReactNode {
     return (
         <main>
             <div className={styles.container}>
-                <h1 className={styles.title}>Chirurgie Dentară</h1>
+                <h1 className={styles.title}>Chirurgie Oral-Maxillofacială</h1>
+                <p className={styles.exam}>It may be exam day but it is your day, so good luck</p>
                 <div className={styles.buttonContainer}>
                     <Link href="/study">
-                        <button className={styles.button}>Study</button>
+                        <button>Study</button>
                     </Link>
                     <Link href="/Quiz">
-                        <button className={styles.button}>Quiz Yourself</button>
+                        <button>Quiz Yourself</button>
                     </Link>
+                </div>
+            </div>
+            <div className={styles.scrollContainer}>
+                <div className={styles.scroll} style={{ "--t": "100s" } as React.CSSProperties}>
+                    <div>
+                        <span>
+                            You&apos;re capable and resilient, believe in
+                            yourself!
+                        </span>
+                        <span>Your hard work will pay off – keep pushing!</span>
+                        <span>Today is your big day so shine</span>
+                        <span>
+                            Each exam is a chance to showcase your growth.
+                        </span>
+                        <span> 
+                            Your efforts will shine brighter than any exam
+                            stress.
+                        </span>
+                        <span>
+                            You&apos;re not alone – we&apos;re here cheering for
+                            you!
+                        </span>
+                        <span>
+                            Stay positive; you&apos;re one step closer to your
+                            goals.
+                        </span>
+                        <span>Your dedication is inspiring; keep it up!</span>
+                        <span>
+                            It&apos;s about the journey, not just the
+                            destination.
+                        </span>
+                    </div>
+                    <div>
+                        <span>
+                            You&apos;re capable and resilient, believe in
+                            yourself!
+                        </span>
+                        <span>Your hard work will pay off – keep pushing!</span>
+                        <span>Today is your big day so shine</span>
+                        <span>
+                            Each exam is a chance to showcase your growth.
+                        </span>
+                        <span> 
+                            Your efforts will shine brighter than any exam
+                            stress.
+                        </span>
+                        <span>
+                            You&apos;re not alone – we&apos;re here cheering for
+                            you!
+                        </span>
+                        <span>
+                            Stay positive; you&apos;re one step closer to your
+                            goals.
+                        </span>
+                        <span>Your dedication is inspiring; keep it up!</span>
+                        <span>
+                            It&apos;s about the journey, not just the
+                            destination.
+                        </span>
+                    </div>
+                </div>
+                <div className={styles.scroll} style={{ "--t": "120s" } as React.CSSProperties}>
+                    <div>
+                        <span>
+                            You&apos;re capable and resilient, believe in
+                            yourself!
+                        </span>
+                        <span>Your hard work will pay off – keep pushing!</span>
+                        <span>Today is your big day so shine</span>
+                        <span>
+                            Each exam is a chance to showcase your growth.
+                        </span>
+                        <span>
+                            {" "}
+                            Your efforts will shine brighter than any exam
+                            stress.
+                        </span>
+                        <span>
+                            You&apos;re not alone – we&apos;re here cheering for
+                            you!
+                        </span>
+                        <span>
+                            Stay positive; you&apos;re one step closer to your
+                            goals.
+                        </span>
+                        <span>Your dedication is inspiring; keep it up!</span>
+                        <span>
+                            It&apos;s about the journey, not just the
+                            destination.
+                        </span>
+                    </div>
+                    <div>
+                        <span>
+                            You&apos;re capable and resilient, believe in
+                            yourself!
+                        </span>
+                        <span>Your hard work will pay off – keep pushing!</span>
+                        <span>Today is your big day so shine</span>
+                        <span>
+                            Each exam is a chance to showcase your growth.
+                        </span>
+                        <span>
+                            {" "}
+                            Your efforts will shine brighter than any exam
+                            stress.
+                        </span>
+                        <span>
+                            You&apos;re not alone – we&apos;re here cheering for
+                            you!
+                        </span>
+                        <span>
+                            Stay positive; you&apos;re one step closer to your
+                            goals.
+                        </span>
+                        <span>Your dedication is inspiring; keep it up!</span>
+                        <span>
+                            It&apos;s about the journey, not just the
+                            destination.
+                        </span>
+                    </div>
                 </div>
             </div>
         </main>
--- app/study/page.tsx
+++ app/study/page.tsx
@@ -13,18 +13,32 @@ const Study = ():React.ReactNode => {
         <div className={styles.container}>
             <div className={styles.introContainer}>
                 <h1 className={styles.title}>Study Hall </h1>
-                <input type="text" className={styles.input} placeholder="Search..." onChange={(e)=>setInputText(e.target.value.toLowerCase())}/>
-            </div>
-            <section className={styles.cardContainer
-            }>
-                {data.filter((el, index)=>el.question.toLowerCase().includes(inputText) || String(index+1).includes(inputText)).map((el, i) => (
-                    <Minicard
-                        title={el.question}
-                        index={el.index}   
-                        key={el.index}
-                        link={`/study/${el.index-1}`}
+                <div className={styles.inputContainer}>
+                    <input
+                        type="text"
+                        className={styles.input}
+                        placeholder="Search card by number or keyword"
+                        onChange={(e) =>
+                            setInputText(e.target.value.toLowerCase())
+                        }
                     />
-                ))}
+                </div>
+            </div>
+            <section className={styles.cardContainer}>
+                {data
+                    .filter(
+                        (el, index) =>
+                            el.question.toLowerCase().includes(inputText) ||
+                            String(index + 1).includes(inputText)
+                    )
+                    .map((el, i) => (
+                        <Minicard
+                            title={el.question}
+                            index={el.index}
+                            key={el.index}
+                            link={`/study/${el.index - 1}`}
+                        />
+                    ))}
             </section>
         </div>
     );
